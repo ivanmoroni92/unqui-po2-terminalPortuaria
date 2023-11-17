fa6f5adb1d3e3b3cc880925c04a63c6c147b6f0e
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-17 15:41:07 -0300

    creando clase y test prueba

--- test/PruebaTest.java
+++ test/PruebaTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 class PruebaTest {
 
 	@BeforeEach
-	void setUp() throws Exception {
-	}
+	void setUp() throws Exception {}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Prueba prueba = new Prueba("probanding");
+		String nombrePrueba = "probanding";
+		assertEquals(nombrePrueba, prueba.getAsd());
 	}
 
 }
