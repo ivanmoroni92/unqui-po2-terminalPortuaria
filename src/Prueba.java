5269c629abd478ec6d4a929bed334001a187e20c
Lucas Taboada <82471007+LucasTaboada111@users.noreply.github.com>
2023-11-17 15:42:16 -0300

    creando clase y test prueba (#1)

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,4 +1,16 @@
 
 public class Prueba {
+    String asd;
 
+    public Prueba(String asd) {
+        this.asd = asd;
+    }
+
+    public String getAsd() {
+        return asd;
+    }
+
+    public void setAsd(String asd) {
+        this.asd = asd;
+    }
 }
