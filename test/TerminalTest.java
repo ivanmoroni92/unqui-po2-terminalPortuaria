1e8c19c3e44c873a093dfeb80e54f0b8df1cb8f4
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-18 19:30:53 -0300

    borramos classpath

--- test/TerminalTest.java
+++ test/TerminalTest.java
@@ -1,9 +1,8 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
-import java.awt.Point;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.awt.*;
+
+
 
 public class TerminalTest {
 	
