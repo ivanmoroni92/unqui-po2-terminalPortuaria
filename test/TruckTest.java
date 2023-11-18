bd73bd746c8c1f3fe1c5abb2dd5e4895f780a59b
ivan <ivan.mor92@gmail.com>
2023-11-18 15:39:05 -0300

    prueba de mock

--- test/TruckTest.java
+++ test/TruckTest.java
@@ -1,13 +1,28 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+import javax.sql.rowset.BaseRowSet;
 
 class TruckTest {
 	// clase que testea que la instancia del truck se cree correctamente
-
+    @Mock private Truck viajarSeguros ;
+  
+    
+    @BeforeEach
+    void setUp() {
+    	viajarSeguros = mock(Truck.class) ;
+		
+    }
 	@Test
 	void testNameEquals() {
 		Truck truck = new Truck("Iveco");
 		String truckName = "Iveco";
+		
 		assertEquals(truckName, truck.getName());
 	}
 
@@ -17,6 +32,13 @@ class TruckTest {
 		Truck truck = new Truck("Iveco2");
 		String truckName = "Iveco";
 		assertNotEquals(truckName, truck.getName());
+	}
+	
+	@Test
+	void testNameIsEqualsMock() {
+		when(viajarSeguros.getName()).thenReturn("viajarSeguros") ;
+		assertEquals("viajarSeguros", viajarSeguros.getName());	
+		
 	}
 
 }
