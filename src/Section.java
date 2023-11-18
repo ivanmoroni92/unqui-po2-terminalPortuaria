b120c87539d5f3ebdf51d740f0255a2ff335a2fc
rodriguez-guillermo <guillermodrod@gmail.com>
2023-11-18 15:39:57 -0300

    Section creada. Falta implementar test

--- src/Section.java
+++ src/Section.java
@@ -1,4 +1,23 @@
+import java.time.LocalDateTime;
 
 public class Section {
-
+	
+	private Terminal startingTerminal;
+	private Terminal endingTerminal;
+	private Double price;
+	private LocalDateTime time;
+	
+	public Terminal getStartingTerminal() {
+		return startingTerminal;
+	}
+	public Terminal getEndingTerminal() {
+		return endingTerminal;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public LocalDateTime getTime() {
+		return time;
+	}
+	
 }
