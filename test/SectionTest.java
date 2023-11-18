61c48578c0c9b80c01171bbd008cdce828a0db8b
rodriguez-guillermo <guillermodrod@gmail.com>
2023-11-18 18:48:12 -0300

    Test getTime y getPrice

--- test/SectionTest.java
+++ test/SectionTest.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
 
 public class SectionTest {
 	
@@ -31,4 +32,40 @@ public class SectionTest {
 		verify(terminalBsAs,times(2)).getName();	//verifica cuantas veces se ejecutaron el metodo que quiero
 	}
 	
+	@Test
+	void testStartingTerminalNotEqualsName() {
+		when(terminalMontevideo.getName()).thenReturn("Montevideo"); //Este es el comportamiento de mi mock, ya que no existe, yo le digo que es lo q quiero que haga
+		assertNotEquals(terminalMontevideo.getName(), sectionTest.getStartingTerminal().getName());
+		
+		verify(terminalBsAs,times(1)).getName();	//verifica cuantas veces se ejecutaron el metodo que quiero
+		verify(sectionTest.getStartingTerminal(),times(1)).getName();
+	}
+	
+	@Test
+	void testEndingTerminalNotEqualsName() {
+		when(terminalMontevideo.getName()).thenReturn("Montevideo"); //Este es el comportamiento de mi mock, ya que no existe, yo le digo que es lo q quiero que haga
+		assertEquals(terminalMontevideo.getName(), sectionTest.getEndingTerminal().getName());
+		
+		verify(terminalMontevideo,times(2)).getName();	//verifica cuantas veces se ejecutaron el metodo que quiero
+	}
+	
+	@Test
+	void testSectionEqualsPrice() {
+		assertEquals(1000.0,sectionTest.getPrice());
+	}
+	
+	@Test
+	void testSectionNotEqualsPrice() {
+		assertNotEquals(100.0,sectionTest.getPrice());
+	}
+	
+	@Test
+	void testSectionEqualsTime() {
+		assertEquals(5,sectionTest.getTime());
+	}
+	
+	@Test
+	void testSectionNotEqualsTime() {
+		assertNotEquals(2,sectionTest.getTime());
+	}
 }
