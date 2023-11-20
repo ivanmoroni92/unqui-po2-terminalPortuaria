805f81ef5bd33b84afd35bc3d085fbe29e8f3de9
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-20 17:47:02 -0300

    shipping company sin test

---
+++ test/ShippingCompanyTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ShippingCompanyTest {
+    @Mock private Ship ship;
+    @Mock private ShippingRoute ruta;
+    @Mock private Travel travel;
+    private ShippingCompany company;
+
+
+}
