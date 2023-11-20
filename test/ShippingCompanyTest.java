e2b9b9b190e21e1fd52e9535a24e430569e65a19
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-20 19:25:21 -0300

    shippingCompanyTest

--- test/ShippingCompanyTest.java
+++ test/ShippingCompanyTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -6,11 +7,51 @@ import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
 public class ShippingCompanyTest {
-    @Mock private Ship ship;
-    @Mock private ShippingRoute ruta;
-    @Mock private Travel travel;
+    @Mock private Ship ship1;
+    @Mock private Ship ship2;
+    @Mock private ShippingRoute route1;
+    @Mock private ShippingRoute route2;
+    @Mock private Travel travel1;
+    @Mock private Travel travel2;
     private ShippingCompany company;
 
+    @BeforeEach
+    void setUp(){
+        travel1 = mock(Travel.class);
+        travel2 = mock(Travel.class);
+        ship1 = mock(Ship.class);
+        ship2 = mock(Ship.class);
+        route1 = mock(ShippingRoute.class);
+        route2 = mock(ShippingRoute.class);
 
+
+
+    }
+
+    @Test
+    void shippingCompanyOk(){
+        List<ShippingRoute> routes = new ArrayList<>();
+        routes.add(route1);
+        routes.add(route2);
+
+        List<Ship> ships = new ArrayList<>();
+        ships.add(ship1);
+        ships.add(ship2);
+
+        List<Travel> schedule = new ArrayList<>();
+        schedule.add(travel1);
+        schedule.add(travel2);
+
+
+        company = new ShippingCompany("CompanyTestName", routes,schedule,ships);
+
+        assertEquals("CompanyTestName", company.getName());
+
+        assertEquals(2,company.getShips().size());
+        assertEquals(2,company.getSchedule().size());
+        assertEquals(2,company.getShippingRoutes().size());
+    }
 }
