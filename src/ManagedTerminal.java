61c960461cb2ec7bf6faf3609be86979c133f7a7
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-21 11:53:06 -0300

    lower price strategy

--- src/ManagedTerminal.java
+++ src/ManagedTerminal.java
@@ -8,6 +8,8 @@ public class ManagedTerminal extends  Terminal {
     private List<Client> consignees; // clientes registrados como consignees habilitados que ya vienen con la temrinal
     private List<Client> shippers; // clientes registrados como shippers habilitados que ya vienen con la temrinal
 
+    private ISearchStrategy strategy; //estrategia de busqueda
+
 
     public ManagedTerminal(String name, Point position, List<Carrier> carriers, ShippingCompany shippingCompany, List<Client> consignees, List<Client> shippers) {
         super(name, position);
@@ -45,6 +47,27 @@ public class ManagedTerminal extends  Terminal {
     public void notifyClients(){
         // por ahora no hace nada
         // metodo que enviara x mail el desglose de servicios y factura a todos los clientes que se relacionan con el buque que cambio de Departing a  Outbound
+    }
+
+    /**
+     * Establece la estrategia de búsqueda para seleccionar el mejor Travel.
+     * @param strategy La estrategia de búsqueda a establecer.
+     */
+    public void setStrategy(ISearchStrategy strategy){
+        this.strategy= strategy;
+    }
+
+    /**
+     * searchByStrategy Busca el mejor Travel en base a la estrategia establecida.
+     * por ahora tenemos 3 estrategias de busqueda.
+     * 1)Menor precio entre la managedTerminal y la destinyTerminal
+     * 2)Menos tiempo total de recorrido entre la managedTerminal y la destinyTerminal.
+     * 3)Menor cantidad de terminales intermedias entre la managedTerminal y la destinyTerminal.
+     * @param destinyTerminal La terminal de destino para la búsqueda.
+     * @return El mejor Travel según la estrategia.
+     */
+    public Travel searchByStrategy( Terminal destinyTerminal){
+       return this.strategy.search(this, destinyTerminal);
     }
 
 }
