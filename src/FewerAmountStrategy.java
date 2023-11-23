638c5a019b3c1eca3e810483a8009029c43507da
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2023-11-23 13:13:57 -0300

    fewerAmountStrategy

--- src/FewerAmountStrategy.java
+++ src/FewerAmountStrategy.java
@@ -1,10 +1,59 @@
-public class FewerAmountStrategy implements ISearchStrategy{
+import java.util.List;
+
+public class FewerAmountStrategy implements ISearchStrategy {
 
     public FewerAmountStrategy() {
     }
 
     @Override
     public Travel search(ManagedTerminal origenTerminal, Terminal destinyTerminal) {
-        return null;
+        List<Travel> travels = origenTerminal.getShippingCompany().getSchedule();
+
+        // Inicializamos las variables para realizar el seguimiento del travel con la menor cantidad de terminales intermedias
+        Travel minIntermediateTerminalsTravel = null;
+        int minIntermediateTerminals = Integer.MAX_VALUE;
+
+        for (Travel travel : travels) {
+            try {
+                int intermediateTerminals = calculateIntermediateTerminals(travel, origenTerminal, destinyTerminal);
+
+                // Actualizamos si encontramos un travel con menos terminales intermedias
+                if (intermediateTerminals < minIntermediateTerminals) {
+                    minIntermediateTerminals = intermediateTerminals;
+                    minIntermediateTerminalsTravel = travel;
+                }
+            } catch (IllegalArgumentException e) {
+                // Manejar la excepción si es necesario
+            }
+        }
+
+        return minIntermediateTerminalsTravel;
+    }
+
+    // Método auxiliar para calcular la cantidad de terminales intermedias entre la terminal de origen y destino
+    private int calculateIntermediateTerminals(Travel travel, ManagedTerminal origenTerminal, Terminal destinyTerminal) {
+        List<Section> sections = travel.getRoute().getSections();
+        int intermediateTerminals = 0;
+
+        boolean reachedOriginTerminal = false;
+
+        for (Section section : sections) {
+            Terminal startingTerminal = section.getStartingTerminal();
+            Terminal endingTerminal = section.getEndingTerminal();
+
+            if (startingTerminal.equals(origenTerminal)) {
+                reachedOriginTerminal = true;
+            }
+
+            if (reachedOriginTerminal) {
+                intermediateTerminals++;
+
+                if (endingTerminal.equals(destinyTerminal)) {
+                    break; // Terminamos de contar terminales al llegar a la terminal de destino
+                }
+            }
+        }
+
+        return intermediateTerminals - 1; // Restamos 1 porque no contamos la terminal de origen
     }
 }
---
+++ test/FewerAmountStrategyTest.java
@@ -0,0 +1,69 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FewerAmountStrategyTest {
+
+    private ManagedTerminal origenTerminal;
+    private Terminal destinyTerminal;
+    private ShippingCompany shippingCompany;
+    private ISearchStrategy fewerAmountStrategy;
+
+    @BeforeEach
+    void setUp() {
+        origenTerminal = mock(ManagedTerminal.class);
+        destinyTerminal = mock(Terminal.class);
+        shippingCompany = mock(ShippingCompany.class);
+        fewerAmountStrategy = new FewerAmountStrategy();
+    }
+
+    @Test
+    void testFewerAmountStrategy() {
+        Travel travel1 = mock(Travel.class);
+        Travel travel2 = mock(Travel.class);
+        Travel travel3 = mock(Travel.class);
+
+        Section section1 = mock(Section.class);
+        Section section2 = mock(Section.class);
+        Section section3 = mock(Section.class);
+
+        // Configuración de secciones en los viajes
+        List<Section> sections1 = Arrays.asList(section1, section2, section3);
+        List<Section> sections2 = Arrays.asList(section1, section2);
+        List<Section> sections3 = Arrays.asList(section1, section2, section3);
+
+        when(travel1.getRoute()).thenReturn(mock(ShippingRoute.class));
+        when(travel1.getRoute().getSections()).thenReturn(sections1);
+        when(section1.getStartingTerminal()).thenReturn(origenTerminal);
+        when(section1.getEndingTerminal()).thenReturn(mock(Terminal.class));
+
+        // el travel 2 es el que menos secciones tiene.
+        when(travel2.getRoute()).thenReturn(mock(ShippingRoute.class));
+        when(travel2.getRoute().getSections()).thenReturn(sections2);
+        when(section2.getStartingTerminal()).thenReturn(origenTerminal);
+        when(section2.getEndingTerminal()).thenReturn(mock(Terminal.class));
+
+        when(travel3.getRoute()).thenReturn(mock(ShippingRoute.class));
+        when(travel3.getRoute().getSections()).thenReturn(sections3);
+        when(section3.getStartingTerminal()).thenReturn(origenTerminal);
+        when(section3.getEndingTerminal()).thenReturn(mock(Terminal.class));
+
+        // Configuración de mocks y relaciones
+        when(origenTerminal.getShippingCompany()).thenReturn(shippingCompany);
+        when(shippingCompany.getSchedule()).thenReturn(Arrays.asList(travel1, travel2, travel3));
+
+        // Uso de la estrategia para buscar el mejor viaje
+        Travel result = fewerAmountStrategy.search(origenTerminal, destinyTerminal);
+
+        // Verificación o aserción del resultado esperado
+        assertEquals(travel2, result);
+    }
+}
+
+
+
