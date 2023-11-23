dc7a5003a463b427d4cb6a84c35ed1fbd05c897b
Iván <69017661+ivanmoroni92@users.noreply.github.com>
2023-11-23 13:14:48 -0300

    implementar el buscador en la terminal (#23)

--- src/ManagedTerminal.java
+++ src/ManagedTerminal.java
@@ -1,15 +1,18 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ManagedTerminal extends  Terminal {
 
     private List<Carrier> carriers;
-    private ShippingCompany shippingCompany;
+    private List<ShippingCompany> shippingCompany;
     private List<Client> consignees; // clientes registrados como consignees habilitados que ya vienen con la temrinal
     private List<Client> shippers; // clientes registrados como shippers habilitados que ya vienen con la temrinal
 
 
-    public ManagedTerminal(String name, Point position, List<Carrier> carriers, ShippingCompany shippingCompany, List<Client> consignees, List<Client> shippers) {
+    public ManagedTerminal(String name, Point position, List<Carrier> carriers, List<ShippingCompany>  shippingCompany, List<Client> consignees, List<Client> shippers) {
         super(name, position);
         this.carriers = carriers;
         this.shippingCompany = shippingCompany;
@@ -21,8 +24,13 @@ public class ManagedTerminal extends  Terminal {
         return carriers;
     }
 
-    public ShippingCompany getShippingCompany() {
+    public List<ShippingCompany> getShippingCompany() {
         return shippingCompany;
+    }
+    
+    
+    public void addShippingCompany(ShippingCompany shippingCompany) {
+    	this.shippingCompany.add(shippingCompany);
     }
 
     public List<Client> getConsignees() {
@@ -45,6 +53,33 @@ public class ManagedTerminal extends  Terminal {
     public void notifyClients(){
         // por ahora no hace nada
         // metodo que enviara x mail el desglose de servicios y factura a todos los clientes que se relacionan con el buque que cambio de Departing a  Outbound
+    }
+    
+    /**
+     * Busca y filtra viajes de todas las compañías de envío según un criterio de búsqueda proporcionado.
+     *
+     * Este método toma un objeto que implementa la interfaz {@code ISearchEngine}, el cual
+     * define un criterio de búsqueda específico. Luego, busca en la lista de viajes de todas
+     * las compañías de envío y filtra los viajes que cumplen con dicho criterio.
+     *
+     * @param search Objeto que implementa {@code ISearchEngine} y proporciona el criterio de búsqueda.
+     * @return Una lista de viajes que cumplen con el criterio de búsqueda especificado.
+     */
+    
+    public List<Travel> searchByCriteria(ISearchEngine search){
+    	// Obtener todos los viajes de todas las compañías de envío
+    	List<Travel> travels = this.shippingCompany.stream().flatMap(company -> company.getSchedule().stream()).collect(Collectors.toList());
+    	List<Travel> filteredTravel = new ArrayList<Travel>() ;
+    	
+    	
+    	for (Travel travel : travels) {
+			if(search.markedFilter(travel)) {
+				filteredTravel.add(travel);
+			}
+		}
+    	
+    	return filteredTravel ;
+    		               
     }
 
 }
